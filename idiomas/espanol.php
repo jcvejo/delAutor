0fd984c1e7bc9c967fd651c09c073465c237bf55
jcvejo <jcvejo@gmail.com>
2020-01-19 13:13:58 -0300

    Add files via upload

---
+++ idiomas/espanol.php
@@ -0,0 +1,44 @@
+<?php
+define("TITLE", "Centro de traducción");
+define("SUBTITULO", "Japonés - Español");
+define("QS", "SOMOS UNA EMPRESA QUE NACIÓ EN ARGENTINA CON EL COMPROMISO DE BRINDAR UN SERVICIO DE TRADUCCIÓN DEL IDIOMA JAPONÉS AL ESPAÑOL.");
+define("QS2","NUESTRA MISIÓN ES PODER COLABORAR EN TUS PROYECTOS, BRINDÁNDOTE LA MÁXIMA CALIDAD. PARA ELLO, CONTAMOS CON UN STAFF CONFORMADO POR EDITORES Y TRADUCTORES ESPECIALIZADOS EN DICHOS LENGUAJES, CON UNA AMPLIA EXPERIENCIA EN TRADUCCIONES CIENTÍFICO- LITERARIAS Y TÉCNICAS.");
+define("QS3","ESTAMOS EN PERMANENTE ACTUALIZACIÓN Y PERFECCIONAMIENTO ASISTIENDO A DIFERENTES EVENTOS Y SEMINARIOS. NOS ESFORZAMOS AL MÁXIMO PARA CUMPLIR CON TUS EXPECTATIVAS Y EXIGENCIAS. POR ESO, CADA TRADUCCIÓN ES UN NUEVO DESAFÍO PARA NOSOTROS.");
+define("SUBTITULO2","Cómo trabajamos");
+define("TAREA1","Traductores especializados");
+define("TAREA2","Localización");
+define("TAREA3","Revisión");
+define("TAREA4","Formato de entrega");
+define("DETALLE1","En Del Autor contamos con traductores especializados en las áreas de informática, ingeniería, ciencias, marketing, prensa y literatura.");
+define("DETALLE2","Adaptamos la traducción de acuerdo al contexto lingüístico, región geográfica y/o público a quien va dirigido el contenido.");
+define("DETALLE3","Nos disponemos a cotejar el original con la versión traducida para lograr un texto lo más fidedigno posible. Luego nos encargamos de analizar y corregir el texto enfocándonos en la cohesión y coherencia, para mejorar y aclarar el mensaje que se quiere transmitir en el texto.");
+define("DETALLE4","El trabajo se entregará en la fecha pactada con el formato y los requisitos que el cliente nos solicite. Todas las traducciones se conservarán en nuestro servidor para que el cliente pueda pedir una copia cada vez que lo necesite, sin costo adicional.");
+define("SUBTITULO3","Lo que hacemos");
+define("TRADUCCIONES1","Traducciones literarias");
+define("TRADUCCIONES2","Traducciones comerciales");
+define("TRADUCCIONES3","Traducciones científico-técnicas");
+define("TRADUCCIONES4","Traducciones web");
+define("ACLARACION1","Novelas, cuentos, biografías y mangas.");
+define("ACLARACION2","Artículos periodísticos, gacetillas de prensa, informes, cartas, documentos contables, institucionales y marketing.");
+define("ACLARACION3","Artículos científicos, académicos, técnicos, monografías, tesis y ensayos.");
+define("ACLARACION4","Videojuegos, páginas web, e-mails, blogs y artículos digitales.");
+define("MENU","Menú");
+define("IDIOMA","Elegí tu idioma");
+define("ELEGIR","Elegir");
+define("SUBTITULOMENU","Cómo lo hacemos");
+define("SUBTITULOMENU2","Quiénes somos");
+define("CONTACTO","Contacto");
+define("COMENTARIO","Por cualquier consulta o presupuesto para tu proyecto, no dudés en comunicarte con nosotros.");
+
+
+
+
+
+
+
+
+
+
+
+
+?>
